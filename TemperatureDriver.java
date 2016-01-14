400c5033c43baebfc8b8dce780ef5706e6098269
prestondes <prestondes@yahoo.com>
2016-01-14 00:04:34 -0500

    Create TemperatureDriver.java

---
+++ TemperatureDriver.java
@@ -0,0 +1,50 @@
+@@ -0,0 +1,48 @@
+import java.util.concurrent.SynchronousQueue;
+
+public class TemperatureDriver
+{
+	public static final int ARRAY_SIZE = 5;
+	public static void main(String[] args)
+	{
+		int x;
+		Temperature temp1 = new Temperature(100.0, 'C');
+		Temperature temp2 = new Temperature(122, 'F');
+		Temperature temp3 = new Temperature(32.0, 'F');
+		Temperature temp4 = new Temperature(100.0, 'C');
+		Temperature tempAve = new Temperature(50.0, 'C');
+		System.out.println(temp2 + " to Celcius is " + temp2.toCelsius());
+		System.out.println("Temp1 is " + temp1);
+		temp1 = temp1.toKelvin();
+		System.out.println("Temp1 to Kalvin is " + temp1);
+		if (temp2.equals(tempAve))
+		{
+			System.out.println("These two temperatures are equal");
+		}
+		else
+		{
+			System.out.println("These two temperature are not equal");
+		}
+		System.out.println("tempAve is " + tempAve);
+		System.out.println("temp1 is " + temp1);
+		System.out.println("temp2 is " + temp2);
+		System.out.println("temp3 is " + temp3);
+		System.out.println("temp4 is " + temp4);
+
+		tempAve = tempAve.add(temp1);
+		System.out.println(tempAve);
+		tempAve = tempAve.add(temp2);
+		System.out.println(tempAve);
+		tempAve = tempAve.add(temp3);
+		System.out.println(tempAve);
+		tempAve = tempAve.add(temp4);
+		System.out.println(tempAve);
+		tempAve = tempAve.divide(5);
+		System.out.println("The average temperatrure is " + tempAve);
+		System.out.print("Subtracting " + temp2 + " from " + temp4 +" gives " );
+		temp4 = temp4.subtract(temp2);
+		System.out.println(temp4);
+		
+	}
+		
+}
+\ No newline at end of file
